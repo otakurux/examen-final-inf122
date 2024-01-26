599662452047970af833dda288a09b5c42b109b5
otakurux <mamanideivid33@gmail.com>
2024-01-26 09:28:55 -0400

    add

---
+++ src/app/components/page.js
@@ -0,0 +1,13 @@
+"use client"
+
+import Pokemon from "./n245";
+
+const page = () => {
+    return (
+        <div className="">
+            <Pokemon />
+        </div>
+    )
+}
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-"use client"
 import React from "react";
 import styles from "./page.module.css";
-import n245 from "./components/n245";
 
 export default function Home() {
   return (
-    <div className="">
-      <n245 />
+    <div className={styles.main}>
+      <h1>pagina principal</h1>
     </div>
   );
 }
